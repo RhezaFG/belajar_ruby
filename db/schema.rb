b6b7bbb488a56de285bae96df5713b66dbc8b767
Rheza FG <rhezafirmansyahg@student.uns.ac.id>
2024-06-25 13:56:27 +0700

    create new db for Authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_122347) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_064145) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
-    t.string "phone"
-    t.string "number"
-    t.string "date"
-    t.string "of"
-    t.date "birth"
+    t.string "phone_number"
+    t.string "date_of_birth"
     t.string "gender"
     t.text "address"
     t.datetime "created_at", null: false
