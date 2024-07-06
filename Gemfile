cf2c9b5ad99b4c77cc9b766587bf2b5b2e856f8a
Rheza FG <rhezafirmansyahg@student.uns.ac.id>
2024-07-06 14:30:41 +0700

    create dropdown & pagination

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  # gem "rails_live_reload"
+  # gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
@@ -67,3 +69,5 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.6"
+gem 'kaminari'
+
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+    @books = Book.order(:title)
+    @books = @books.page params[:page]
   end
 
   # GET /books/1 or /books/1.json
@@ -65,6 +66,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :author, :publication_year)
+      params.require(:book).permit(:title, :author_id, :publication_year)
     end
 end
